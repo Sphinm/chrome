33b7bce94fbaf8f2a7f89b979ee931abd59b1e72
sumin <1500160640@qq.com>
2018-01-19 13:43:35 +0800

    新增微博和知乎的去广告
    新增微博和知乎的去广告以及清除百度的二次广告

--- main.js
+++ main.js
@@ -5,18 +5,17 @@
      *  由于百度的的广告是异步加载的，然而插件是dom加载完成就执行了，所以需要手动的刷新一下，很不方便
      *  解决办法：通过设置一个定时器来模拟手动刷新
      *
-     *  lRLJvx ekrJHU vOxVLk fXKqeY OGQyZL / sUdrNZ fdNwIe BUAeou CunVev H_Tqks
-     *  上面是两个底部广告的类名，没有什么规律
+     *  目前只能针对当前页 加载到下一页还是需要手动刷新一下,对于点击页码和点击上下页还需要修改
      * */
 
     let ad = {
-        adSafe: function () {
+        baidu: function () {
 
             // 右侧区域广告
             let cr = document.querySelector('.cr-offset');
             if (cr) cr.remove();
 
-            // 删除顶部第一个节点强制显示的广告
+            // 删除顶部和底部强制显示的广告
             let cnt = document.querySelector('#content_left');
             let css_first = cnt.firstElementChild.style.cssText;
             let css_last = cnt.lastElementChild.style.cssText;
@@ -24,10 +23,6 @@
             if (css_first === temp) cnt.firstElementChild.remove();
             if (css_last === temp) cnt.lastElementChild.remove();
 
-
-            // 顶部强制显示的广告删除后默认会显示到最后一个，还需要再删除一下
-            let clone = document.querySelector('#clone');
-            if (clone) clone.remove();
 
             // 删除有该类名的广告列表
             let outer = document.querySelectorAll('.ec_tuiguang_ppouter');
@@ -46,24 +41,83 @@
                     link[i].parentNode.parentNode.parentNode.remove()
                 }
             }
+
+
+            // 将第二次ajax加载出的数据中的广告再次清除
+            setTimeout(function () {
+                let after = document.querySelectorAll('#content_left .result[id="1"] .f13 span.m');
+                for (let i=0 ;i<after.length; i++) {
+                    if (after[i].innerText === '广告'){
+                        after[i].parentNode.parentNode.remove();
+                    } else
+                        console.log('没有执行第二次ajax清除')
+                }
+            },2000)
+        },
+
+
+        // 去除微博首页的广告，下拉后异步加载的数据暂时无法去掉
+        weibo: function(){
+            // 这里是微博的广告，不过就只有一点
+            console.log('进入weibo');
+            let wb = document.querySelector('.WB_feed div[data-mark]');
+            if (wb) wb.remove();
+        },
+
+        // 去除知乎首页的广告，下拉后异步加载的数据暂时无法去掉
+        zhihu: function () {
+            console.log('进入zhihu');
+          let zh = document.querySelector('.TopstoryItem--advertCard');
+          if (zh) zh.remove();
         },
 
         // 通过设置定时器来达到手动刷新的效果
         removeAd: function () {
-            let time1 = setTimeout(ad.adSafe, 1000);
+            // 初始化执行
+            let time1 = setTimeout(ad.baidu, 1000);
             let time2 = setTimeout(function(){}, 1000);
 
             // 监听搜索button的点击事件
             document.querySelector('#su').addEventListener('click', function () {
-                time1 = setTimeout(ad.adSafe, 1000);
+                time1 = setTimeout(ad.baidu, 1000);
             });
+
             // 监听输入框的input事件
             document.querySelector('#kw').addEventListener('input', function () {
-                time2 = setTimeout(ad.adSafe, 1000);
+                time2 = setTimeout(ad.baidu, 1000);
             });
+
+
+            // // 监听上一页、下一页的click事件
+            // document.querySelector('#page .n').addEventListener('click', function () {
+            //     console.log(123)
+            //     time2 = setTimeout(ad.baidu, 2000);
+            // });
+
+            // // 监听页码的点击事件
+            // let page = document.querySelectorAll('#page .pc');
+            // for (let i=0; i<page.length; i++) {
+            //     page[i].addEventListener('click', function () {
+            //         console.log(1234)
+            //         time2 = setTimeout(ad.baidu, 1000);
+            //     });
+            // }
+
+
         }
     };
 
     // 执行
-    ad.removeAd();
+    if(window.location.href.indexOf('baidu') > -1){
+        ad.removeAd();
+    }
+
+    if (window.location.href.indexOf('weibo') > -1) {
+        ad.weibo();
+    }
+    if (window.location.href.indexOf('zhihu') > -1) {
+        ad.zhihu();
+    }
+
+
 })();
